61c3704b9f47a521c920e287c1bd180f2bc8edd9
AnasAlkhamis <anas.alkhamnis@gmail.com>
2021-12-04 11:45:39 +0300

    Project_1_steps

--- main.js
+++ main.js
@@ -1,74 +1,55 @@
 /** @format */
+// dom element
 
 const pictures = document.querySelectorAll(".pictures div");
 const img = document.createElement("img");
 const imgs = document.querySelectorAll(".pictures div img");
+const rightMove = document.querySelector(".right spam");
+const wrongMove = document.querySelector(".wrong spam");
+const score = document.querySelector(".score spam");
+const timer = document.querySelector(".timer spam");
+
+//  function check the Image
 
 const onClickimg = [];
+let correct = 0;
+let notCorrect = 0;
+let cooldown = 0;
 const removeElements = () => {
   for (let i = 0; i < pictures.length; i++) {
     pictures[i].addEventListener("click", (e) => {
       e.target.firstChild.style.display = "unset";
       e.target.classList = "onClick";
       onClickimg.push(e.target.firstChild.src);
-      console.log(e);
-      const onTarget = document.querySelectorAll(".onClick");
+      //if the first click === the second click
       if (onClickimg[0] === onClickimg[1]) {
-        console.log(onTarget);
+        correct++;
         onClickimg.splice(0, 2);
+        const onTarget = document.querySelectorAll(".onClick");
         onTarget.forEach((element) => {
           setTimeout(() => {
             element.remove();
+            udate()
           }, 500);
         });
-      }
-      else if (onClickimg[0] !== onClickimg[1] && onClickimg.length >1){
-          setTimeout(() => {
-            imgs.forEach(element => {
-                element.style.display = "none";
-            })
-            onClickimg.splice(0, 2);
-            e.target.classList = "";
-          }, 500);
-        
+      } else if (onClickimg[0] !== onClickimg[1] && onClickimg.length > 1) {
+        notCorrect++;
+        setTimeout(() => {
+          imgs.forEach((element) => {
+            element.style.display = "none";
+            udate()
+          });
+          onClickimg.splice(0, 2);
+          e.target.classList = "";
+        }, 500);
       }
     });
   }
-
-  /////////////////////////////////////
-  //   pictures.forEach((element) => {
-  //     element.addEventListener("click", (e) => {
-  //       e.target.firstChild.style.display = "unset";
-  //       e.target.classList = "onClick";
-  //       event = e
-  //       onClickimg.push(e.target.firstChild.src);
-  //       console.log(e);
-  //       const onTarget = document.querySelectorAll(".onClick");
-  //       if (onClickimg[0] === onClickimg[1]) {
-  //         console.log(onTarget);
-  //         onClickimg.splice(0, 2);
-  //         onTarget.forEach((element) => {
-  //           setTimeout(() => {
-  //             element.remove();
-  //           }, 500);
-  //         });
-  //       }
-  //     });
-  //   });
 };
-
 removeElements();
-// else
 
-// if (events) {
-//   console.log(e);
-//   onTarget.forEach((element) => {
-//     setTimeout(() => {
-//       element.classList = "";
-//       imgs.forEach((element) => {
-//         console.log("hi");
-//         element.style.display = "none";
-//       });
-//     }, 500);
-//   });
-// }
+const udate = () => {
+  rightMove.innerText = correct;
+  wrongMove.innerText = notCorrect;
+}
+
